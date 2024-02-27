6a7a558937da4222e5f8c48095726b5b16355838
Ritika <writetoritika@gmail.com>
2024-02-26 22:27:01 -0600

    Made changes

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "ajay-mrcloudbook777" # Replace with your actual S3 bucket name
+    bucket = "writetoritika" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
-    region = "ap-south-1"
+    region = "us-east-2"
   }
 }
